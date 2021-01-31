255d3fab252efbb98090d5f170aecb21919213e7
iori-oiso <iori.dazo@gmail.com>
2021-01-31 14:27:19 +0900

    init

---
+++ src/main/java/com/github/ioridazo/spring/batch/sample/SpringBatchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ioridazo.spring.batch.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchSampleApplication.class, args);
+    }
+
+}
